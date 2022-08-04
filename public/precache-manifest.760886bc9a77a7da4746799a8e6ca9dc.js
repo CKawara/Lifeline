bf9e55b33463dd88378f746083343e03905bdd35
Catherine Kawara <ndutakawara2@gmail.com>
2022-08-04 19:45:44 +0300

    add client side routing

---
+++ public/precache-manifest.760886bc9a77a7da4746799a8e6ca9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3f0586ed94cf4e66ad",
+    "url": "/static/css/main.d42e19dc.chunk.css"
+  },
+  {
+    "revision": "4d3f0586ed94cf4e66ad",
+    "url": "/static/js/main.4d3f0586.chunk.js"
+  },
+  {
+    "revision": "d213fccef9b08673a4c8",
+    "url": "/static/js/1.d213fcce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e292325b8ae726ec86284f5631be417",
+    "url": "/static/media/heart.2e292325.png"
+  },
+  {
+    "revision": "79c4ac92681bb6ac71609f918ddc171f",
+    "url": "/index.html"
+  }
+];
